3ad30712d5e4a5527028938d04a731233f8d569f
Lokesh <i.lokesh@gmail.com>
2021-04-13 23:07:21 +0530

    Linked List & Deletions by position

--- sll/deletions.py
+++ sll/deletions.py
@@ -6,7 +6,13 @@ llist.append("B")
 llist.append("C")
 llist.append("D")
 
+print("Deletions by value")
 llist.delete_node("B")
 llist.delete_node("E")
+
+llist.print_list()
+
+print("Deletions by Position")
+llist.delete_node_at_pos(0)
 
 llist.print_list()
