fe00c5bc71247e22d9c6682f7f82ed50b8a4ff8d
Lokesh <i.lokesh@gmail.com>
2021-04-13 23:01:45 +0530

    Linked List & Deletions by value

---
+++ sll/deletions.py
@@ -0,0 +1,12 @@
+from linkList import *
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+
+llist.delete_node("B")
+llist.delete_node("E")
+
+llist.print_list()
--- sll/linkList.py
+++ sll/linkList.py
@@ -38,4 +38,24 @@ class LinkedList:
     new_node.next = prev_node.next
     prev_node.next = new_node
 
+
+  def delete_node(self, key):
+
+     cur_node = self.head
+
+     if cur_node and cur_node.data == key:
+         self.head = cur_node.next
+         cur_node = None
+         return
+
+     prev = None
+     while cur_node and cur_node.data != key:
+         prev = cur_node
+         cur_node = cur_node.next
+
+     if cur_node is None:
+         return
+
+     prev.next = cur_node.next
+     cur_node = None
   
