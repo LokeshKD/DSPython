6a75023b1cda4b77b05092067613dbfed4637ef9
Lokesh <i.lokesh@gmail.com>
2021-04-15 10:58:03 +0530

    Binary Tree and Traversal

---
+++ bt/traversal.py
@@ -0,0 +1,23 @@
+from bTree import *
+
+# 1-2-4-5-3-6-7-
+# 4-2-5-1-6-3-7
+# 4-2-5-6-3-7-1
+#               1
+#           /       \
+#          2          3
+#         /  \      /   \
+#        4    5     6   7
+
+# Set up tree:
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+
+print(tree.print_tree("preorder"))
+print(tree.print_tree("inorder"))
+print(tree.print_tree("postorder"))
