0e41146901001f7203a5a0ea0d8de52de79fbe71
Lokesh <i.lokesh@gmail.com>
2021-04-15 11:22:11 +0530

    Height of BT

---
+++ bt/height.py
@@ -0,0 +1,16 @@
+from bTree import *
+
+# Calculate height of binary tree:
+#     1
+#    / \
+#   2  3
+#  / \
+# 4  5
+#
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+
+print(tree.height(tree.root))
