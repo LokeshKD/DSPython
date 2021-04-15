6c8afd0b5f55c5635ad9b1e42c854b619aef4a2b
Lokesh <i.lokesh@gmail.com>
2021-04-15 16:42:56 +0530

    ChainMap consolidated

--- collections/exChainMap.py
+++ collections/exChainMap.py
@@ -18,7 +18,20 @@ def main():
                      app_defaults)
     print(chain['username'])
 
+def cars():
+    car_parts = {'hood': 500, 'engine': 5000, 'front_door': 750}
+    car_options = {'A/C': 1000, 'Turbo': 2500, 'rollbar': 300}
+    car_accessories = {'cover': 100, 'hood_ornament': 150, 'seat_cover': 99}
+
+    car_pricing = ChainMap(car_accessories, car_options, car_parts)
+
+    print (car_pricing['hood'])
+    #500
+
+
 if __name__ == '__main__':
     main()
     os.environ['username'] = 'test'
     main()
+    cars()
+
