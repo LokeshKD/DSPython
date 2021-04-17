b2a5246e9c3b9318a05f043f0c5f7efc1e4501e7
Lokesh <i.lokesh@gmail.com>
2021-04-17 14:52:07 +0530

    multi-procesing, locks ...

---
+++ multi-pocess/pool.py
@@ -0,0 +1,23 @@
+from multiprocessing import Pool
+
+
+def doubler(number):
+    return number * 2
+
+if __name__ == '__main__':
+    numbers = [5, 10, 20]
+    pool = Pool(processes=3)
+    print(pool.map(doubler, numbers))
+
+'''
+from multiprocessing import Pool
+
+
+def doubler(number):
+    return number * 2
+
+if __name__ == '__main__':
+    pool = Pool(processes=3)
+    result = pool.apply_async(doubler, (25,))
+    print(result.get(timeout=1))
+'''
